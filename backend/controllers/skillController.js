6e2a622a68daa252e0f0b70d013c15595c94f19e
ellen.kuo <ellen.kuo@patrioimemory.com>
2026-08-26 12:19:57 +0800

    feat: 完成新增、查詢、刪除skill功能

--- backend/controllers/skillController.js
+++ backend/controllers/skillController.js
@@ -1,8 +1,55 @@
-import { prisma } from "../lib/prisma.js"
-import errorHander from "../utils/errorHandler.js"
+import { prisma } from "../lib/prisma.js";
+import validator from "validator";
+import { isValidString } from "../utils/validation.js";
+import errorHandler from "../utils/errorHandler.js";
 
-export const getCoachSkill = async(req, res, next) => {
-  const skills = await prisma.skill.findMany()
+export const getCoachSkill = async (req, res, next) => {
+  const skills = await prisma.skill.findMany();
 
-  res.status(200).json({ status:"success", data: skills })
-}
+  res.status(200).json({ status: "success", data: skills });
+};
+
+export const createCoachSkill = async (req, res, next) => {
+  const { name } = req.body;
+
+  if (!isValidString(name)) {
+    return next(errorHandler(400, "欄位未填寫正確"));
+  }
+
+  const hasSkill = await prisma.skill.findUnique({ where: { name } });
+
+  if (hasSkill) {
+    return next(errorHandler(409, "資料重複"));
+  }
+
+  const newSkill = await prisma.skill.create({
+    data: { name },
+    select: {
+      id: true,
+      name: true,
+      created_at: true,
+    },
+  });
+
+  res.status(201).json({ status: "success", data: newSkill });
+};
+
+export const deleteCoachSkill = async (req, res, next) => {
+  const { skillId } = req.params;
+
+  if (!validator.isUUID(skillId)) {
+    return next(errorHandler(400, "ID錯誤"));
+  }
+
+  const hasSkill = await prisma.skill.findUnique({
+    where: { id: skillId.trim() },
+  });
+
+  if (!hasSkill) {
+    return next(errorHandler(400, "ID錯誤"));
+  }
+
+  await prisma.skill.delete({ where: { id: skillId.trim() } });
+
+  res.status(200).json({ status: "success", data: null });
+};
--- backend/routes/skill.js
+++ backend/routes/skill.js
@@ -1,8 +1,10 @@
 import express from "express"
-import { getCoachSkill } from "../controllers/skillController.js"
+import { getCoachSkill, createCoachSkill, deleteCoachSkill } from "../controllers/skillController.js"
 
 const router = express.Router()
 
 router.get("/", getCoachSkill)
+router.post("/", createCoachSkill)
+router.delete("/:skillId", deleteCoachSkill)
 
 export default router
